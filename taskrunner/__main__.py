226abd2fa6f4a21faea5b366e0a33f2727cb35d1
Paul Glushak <paul@glushak.net>
2023-12-01 00:06:05 -0600

    Upstream changes

--- taskrunner/__main__.py
+++ taskrunner/__main__.py
@@ -1,23 +1,7 @@
-import argparse
-import logging
-from taskrunner import TaskRunner, logger
+from . import run
 
-parser = argparse.ArgumentParser(
-    prog="TaskRunner", description="Simple, sequential task runner"
-)
-
-parser.add_argument("-v", "--verbose", action="store_true")
-parser.add_argument("-q", "--quiet", action="store_true")
-parser.add_argument("taskfile")
-
-args = parser.parse_args()
-
-if args.verbose:
-    logger.setLevel(logging.DEBUG)
-
-def run():
-    tr = TaskRunner(args.taskfile, quiet=args.quiet)
-    tr.run()
-    
-if __name__ == "__main__":
+def main():
     run()
+
+if __name__ == '__main__':
+    main()
--- taskrunner/main.py
+++ taskrunner/main.py
@@ -1,4 +1,3 @@
-import copy
 from dataclasses import dataclass, field, replace
 import os
 from pathlib import Path
@@ -6,36 +5,51 @@ import re
 import subprocess
 import sys
 from typing import List, Dict
+from importlib.metadata import version
+from datetime import datetime
 
-# from pprint import pprint as print #TODO: Remove
 from collections import ChainMap
-from termcolor import colored
 
 import logging
 import yaml
 
 logger = logging.getLogger(__name__)
-# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-# logger.setLevel(logging.DEBUG)
+
+
+class color:
+    PURPLE = "\033[95m"
+    GRAY = "\033[2m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
 
 
 class TaskRunner:
 
-    quiet: bool = False
+    _quiet: bool = False
+    _dry_run: bool = False
+    tasks: List["Task"] = []
+    variables: dict = {}
+    helpers: Dict[str, "Helper"] = {}
 
-    def __init__(self, task_file: str, quiet: bool = False) -> None:
-        self.quiet = quiet
+    def __init__(
+        self, task_file: str, quiet: bool, dry_run: bool, text_only: bool
+    ) -> None:
         self._decide_logger_format()
-        logger.info(f"TaskRunner Initialized with file {task_file}")
+        TaskRunner.quiet = quiet
+        TaskRunner.dry_run = dry_run
+        TaskRunner.text_only = text_only
         task_path = Path(task_file)
         if not task_path.exists():
-            logger.error(f"{task_file} doesn't exist. Aborting!")
+            logger.error(f"{task_path} doesn't exist. Aborting!")
             exit(1)
         self.task_path = task_path
-        self.tasks: List["Task"] = []
-        self.variables: dict = {}
-        self.helpers: Dict[str, "Task"] = {}
-        self._parse_taskfile()
 
     def _decide_logger_format(self):
         if logger.getEffectiveLevel() >= 20:
@@ -47,6 +61,9 @@ class TaskRunner:
     def _parse_taskfile(self) -> bool:
         """Load the YAML file and parse the sections within."""
         parsed_yaml = yaml.safe_load(self.task_path.open(encoding="UTF-8"))
+        if "information" in parsed_yaml:
+            if isinstance(parsed_yaml["information"], str):
+                logger.info(f"Information: {parsed_yaml['information'].strip()}")
         if "variables" in parsed_yaml:
             self._populate_variables(parsed_yaml["variables"])
         if "helpers" in parsed_yaml:
@@ -56,43 +73,43 @@ class TaskRunner:
         return parsed_yaml
 
     def _populate_tasks(self, tasks: list):
-        logger.info(f"{len(tasks)} Task(s) found. Populating...")
+        logger.info(f"Tasks: {color.BOLD}{len(tasks)}{color.END}")
         for name, config in tasks.items():
-            task = Task(name=name, **config)
-            task.command = self._parse_variables_in_command(task.command)
-            print(task.command)
-            task.prerequisite_list = self._parse_prerequisites_in_task(
-                task.prerequisites
-            )
-            self.tasks.append(task)
+            self.tasks.append(Task(name=name, **config))
             logger.debug(f"Task {name} added.")
         pass
 
     def _populate_variables(self, variables: List[dict]):
-        logger.info(f"{len(variables)} Variable(s) found. Populating...")
+        logger.info(f"Variables: {color.BOLD}{len(variables)}{color.END}")
         for name, value in ChainMap(*variables).items():
-            self.variables[name] = value.rstrip()
+            self.variables[name] = value
             logger.debug(f"Variable {name} added.")
 
     def _populate_helpers(self, helpers: list):
-        logger.info(f"{len(helpers)} Helper(s) found. Populating...")
+        logger.info(f"Helpers: {color.BOLD}{len(helpers)}{color.END}")
         for name, config in helpers.items():
             self.helpers[name] = Helper(name=name, **config)
             logger.debug(f"Helper {name} added.")
 
-    def _get_variable(self, variable: re.Match) -> str:
-        print(self.variables.get(variable.group(1), ""))
-        return self.variables.get(variable.group(1), "")
+    # def _get_variable(self, variable):
+    #     # print(self.variables.get(variable.group(1), ""))
+    #     return self.variables.get(variable.group(1), "")
 
-    def _parse_variables_in_command(self, command: str):
-        variable_pattern = r"(?:{(.*?)})"
-        return re.sub(variable_pattern, self._get_variable, command)
+    # def _parse_variables_in_command(self, command: str):
+    #     variable_pattern = r"(?:{(.*?)})"
+    #     return re.sub(variable_pattern, self._get_variable, command)
+
+    def _parse_each_loop(self, items: str | list) -> list:
+        if isinstance(items, str):
+            var = self._parse_variables_in_command(items)
+            return self._get_variable(var)
+        return items
 
     def _get_prerequisite(self, prerequisite: str):
         pass
 
     def _parse_prerequisites_in_task(self, prerequisites: str):
-        prerequisite_list = prerequisites.split(" ")
+        prerequisite_list = prerequisites.split(" ") if prerequisites != "" else []
         for i, prerequisite in enumerate(prerequisite_list):
             if prerequisite.startswith("helpers"):
                 helper_pattern = r"(?:helpers\.)(\w+)(?:\((.*?)\))?"
@@ -103,65 +120,181 @@ class TaskRunner:
         return prerequisite_list
 
     def run(self):
-        logger.info("Start running tasks...")
+        if TaskRunner.quiet:
+            logger.setLevel(logging.ERROR)
+        logger.info(
+            f"{color.BOLD}{color.DARKCYAN}[TaskRunner {version(__package__)}]{color.END}"
+        )
+        logger.info(f"Task File: {color.BOLD}{self.task_path.absolute()}{color.END}")
+        if TaskRunner.dry_run:
+            logger.info(f"Dry Run: {color.BOLD}True{color.END}")
+        self._parse_taskfile()
+        logger.info("---")
+        logger.info(f"Started: {color.BOLD}{datetime.today()}{color.END}")
         for task in self.tasks:
-            task.run()
+            prerequisites = self._parse_prerequisites_in_task(task.prerequisites)
+            for prerequsite in prerequisites:
+                if not isinstance(prerequsite, Helper):
+                    continue
+                if not prerequsite._run():
+                    prerequsite.error(
+                        f"Stopping because prerequisite {prerequsite.name} failed.",
+                        fatal=True,
+                    )
+            task._run()
+        logger.info(f"Ended: {color.BOLD}{datetime.today()}{color.END}")
+
+
+class Executable:
+    name: str
+    text: str = ""
+    run: str = ""
+    each: list = []
+
+    def _orun(self):
+        """
+        Parse the string under `each`, in case the command needs to iterate through a list.
+        """
+        # FIXME - Temporary function name to handle the `each` cycle.
+        # TODO - Consider setting `self.commands` directly from here.
+        if each := getattr(self, "each"):
+            if isinstance(self.each, str):
+                each = self.parse_variable(self.each)
+                if not each:
+                    self.error(
+                        f"Value for 'each' under {self.name} is not correct!", True
+                    )
+            self.each = each
+
+    def announce(self):
+        """
+        Print out the task text, in case we're not in quiet mode.
+        """
+        # FIXME - Add check for text only mode as well.
+        if not TaskRunner.quiet and self.text:
+            logger.info(f"{color.UNDERLINE}{self.__class__.__name__} {self.name}{color.END} - {self.text}")
+
+    def error(self, message: str = "", fatal: bool = False):
+        """
+        Print an error message using `logger` with the task name as prefix.
+
+        Args:
+            message (str, optional): The error message. Defaults to "".
+            fatal (bool, optional): Should we exit? Defaults to False.
+        """
+        logger.error(f"{self.name} - {message}")
+        if fatal:
+            exit(1)
+
+    def parse_variable(self, _input: str):
+        variable_pattern = r"(?:variables\.([a-zA-Z-9_]+))"
+        match = re.findall(variable_pattern, _input)
+        return TaskRunner.variables.get(match[0]) if match else None
+
+    def parse_variables_in_str(self, _input: str | list) -> str | None:
+        variable_pattern = r"(?:variables\.([a-zA-Z-9_]+))"
+        if isinstance(_input, str):
+            return re.sub(variable_pattern, self.get_variable, _input)
+        elif isinstance(_input, list):
+            for index, item in enumerate(_input):
+                _input[index] = re.sub(variable_pattern, self.get_variable, item)
+            return _input
+
+    def get_variable(self, variable: str | re.Match):
+        if isinstance(variable, re.Match):
+            variable = variable.group(1)
+        var = TaskRunner.variables.get(variable)
+        if var is None:
+            self.error(f"Variable {variable} doesn't exist!")
+        return var
 
 
 @dataclass
-class Task:
+class Task(Executable):
     name: str
-    command: str = ""
     text: str = ""
+    run: list | str = ""
+    """The command this task needs to run. `str` for shell, `list` for no shell."""
+    commands: list = field(default_factory=list)
+    each: str | list = field(default_factory=list)
     prerequisites: str = ""
     prerequisite_list: list = field(default_factory=list)
     check: str = ""
     success: int = 0
-    shell: bool = False
     cwd: str = os.getcwd()
     env: bool = False
+    show_output: bool = False
+    output: list = field(default_factory=list)
+    """Store the command's output."""
+    require_input: str | bool = False
+    """The message we should show, in case input is required. Display default message if True."""
+    user_input: str = ""
+    """Store user input result from `require_input`."""
+    on_failure: str = ""
 
-    def run(self):
+    def _run(self):
+        self._orun()  # FIXME - Temporary
         logger.debug(f"Starting task {self.name}")
-        self.stdout = ""
-        self._announce_task()
-        if self.prerequisite_list[0] != "":
-            logger.debug(f"Task has {len(self.prerequisite_list)} prerequisite(s)")
-            for prerequisite in self.prerequisite_list:
-                if not prerequisite.run():
-                    exit(f"Prerequisite {prerequisite.name} failed!")
-        if not self.command:
+        if self.each:
+            for iteration in self.each:
+                formatted_command = self.run.format(iteration)
+                self.commands.append(formatted_command)
+        else:
+            self.commands = [self.run]
+        self.announce()
+        if self.require_input:
+            TaskRunner.variables[f"{self.name}_input"] = input(
+                self.require_input
+                if isinstance(self.require_input, str)
+                else "Press ENTER to continue."
+            )
+        if not self.run:
+            # NOTE - Stop if there's nothing to run
             return
-        command = self.command if self.shell else self.command.split(" ")
-        self.cwd = Path(self.cwd).expanduser()
-        logger.debug(f"Running (Shell: {self.shell}, CWD: {self.cwd}): {command}")
-        proc = subprocess.run(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            shell=self.shell,
-            cwd=self.cwd,
-            env=os.environ if self.env else None,
-        )
-        from pprint import pprint
-
-        pprint(proc.stdout.decode())
-        # for line in iter(proc.stdout.readline, b''):
-        #     logger.debug(line.decode())
-        #     self.stdout += line.decode()
-        self._check_output(proc.stdout.decode())
+        for command in self.commands:
+            self.cwd = Path(self.cwd).expanduser()
+            shell = not isinstance(command, list)
+            command = self.parse_variables_in_str(command)
+            # FIXME - Take into account `run` can be a list, so go over list items to format them.
+            run_message = f"(Shell: {shell}, CWD: {self.cwd}): {command}"
+            if TaskRunner.dry_run:
+                logger.info(f"{color.GRAY}DRY RUN {run_message}{color.END}")
+                return True
+            else:
+                logger.debug(f"Running {run_message}")
+            proc = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=shell,
+                cwd=self.cwd,
+                env=os.environ if self.env else None,
+            )
+            while True:
+                line: str = proc.stdout.readline().decode().rstrip()
+                if line == "" and proc.poll() is not None:
+                    break
+                self.output.append(line)
+                if self.show_output:
+                    logger.info(line)
+                else:
+                    logger.debug(line)
+            # logger.debug(proc.stdout.decode())
+            TaskRunner.variables[f"{self.name}_output"] = "\n".join(self.output)
+            self._check_output()
+            if not proc.returncode == self.success:
+                error = "\n".join(
+                    line.rstrip().decode() for line in proc.stderr.readlines()
+                )
+                logger.error(f'Command "{command}" failed ({proc.returncode}): {error}')
         return proc.returncode == self.success
 
-    def _announce_task(self):
-        """Print out task's text, unless quiet has been set."""
-        if TaskRunner.quiet:
-            return
-        logger.info(f"Task {self.name} - {self.text}")
-
-    def _check_output(self, output) -> bool:
+    def _check_output(self) -> bool:
+        command_output = "\n".join(self.output)
         if self.check != "":
             rx = re.compile(self.check)
-            if not re.findall(rx, output):
+            logger.debug(f"Checking for {rx.pattern}")
+            if not re.findall(rx, command_output):
                 logger.error("Check Failed.")
                 return False
         logger.debug(f'"{self.check}" is present in output')
@@ -169,33 +302,38 @@ class Task:
 
 
 @dataclass
-class Helper:
+class Helper(Executable):
     name: str
-    command: str = ""
+    run: str = ""
     text: str = ""
     _args: list = field(default_factory=list)
     shell: bool = False
+    success: int = 0
 
-    def run(self) -> bool:
+    def _run(self) -> bool:
         logger.debug(f"Starting helper {self.name}")
-        self._announce_helper()
-        command = self.command.format(*self._args)
+        self.announce()
+        command = self.run.format(*self._args)
         command = command if self.shell else command.split(" ")
-        logger.debug(f"Running helper {command}")
+        run_message = f"(Shell: {self.shell}): {command}"
+        if TaskRunner.dry_run:
+            logger.info(f"{color.GRAY}DRY RUN {run_message}{color.END}")
+            return True
+        else:
+            logger.debug(f"Running {run_message}")
         proc = subprocess.run(
             command,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            universal_newlines=True,
             shell=self.shell,
         )
-        # for line in iter(proc.stdout.readline, b''):
-        #     logger.debug(line)
-        # proc.stdout.close()
-        if proc.returncode == 0:
+        if proc.returncode == self.success:
             return True
-        logger.error(f"{self.name} failed with {proc.returncode}")
-        print(proc.stderr)
+        logger.error(
+            f"{self.name} failed with {proc.returncode}: {proc.stdout.decode()}"
+        )
+        return False
+        # print(proc.stderr)
 
     def _announce_helper(self):
         if TaskRunner.quiet:
